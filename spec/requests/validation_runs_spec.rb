8539f681076c7ea3ce337f16da25b21baaf6e453
Mathew Button <mat@pushbutton.xyz>
2022-05-19 22:38:04 +1000

    Set request spec to pending

--- spec/requests/validation_runs_spec.rb
+++ spec/requests/validation_runs_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "ValidationRuns", type: :request do
-  describe "GET /show" do
+  xdescribe "GET /show" do
     it "returns http success" do
       get "/validation_runs/show"
       expect(response).to have_http_status(:success)
