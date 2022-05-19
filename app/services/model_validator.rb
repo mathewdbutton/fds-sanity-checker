988f320c869b1cd212a9f61b3626c815d123bcd8
Mathew Button <mat@pushbutton.xyz>
2022-05-19 22:38:29 +1000

    Commented out bulk update

--- app/services/model_validator.rb
+++ app/services/model_validator.rb
@@ -5,10 +5,17 @@ class ModelValidator
     cleaned_lines = ModelReader.new(file).call
       .then { |text_blob| LineUnbundler.new(text_blob).call }
 
-    cleaned_lines.map do |cleaned_line|
+    input_records = cleaned_lines.map do |cleaned_line|
       NamelistExtractor.call(cleaned_line)
         .then { |namelist_tuple| NamelistFactory.call(namelist_tuple) }
         .then { |mapper, namelist_tuple| mapper.call(namelist_tuple, CurrentValidation.validation_run) }
     end
+
+    # groups = input_records.compact.group_by(&:class)
+    # groups.each do |clazz, input_records|
+    #   clazz.insert_all(input_records.map(&:attributes))
+    # end
+    # binding.irb
+    input_records.compact.each(&:save)
   end
 end
