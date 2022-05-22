9d461f6f5a0838c9901267a1a3ca01c36772f443
Mathew Button <mat@pushbutton.xyz>
2022-05-22 21:45:11 +1000

    Add default route to loader

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'validation_runs/show'
-  resources :model_loader
+  resources :model_loader, only: [:create]
+  root "model_loader#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
