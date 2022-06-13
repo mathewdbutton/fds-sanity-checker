f9e69feb5cd9bb4b33d147f782da24c7b3aa54ae
Mathew Button <mat@pushbutton.xyz>
2022-06-13 16:03:52 +1000

    Simplify graph code
    Instead of calcuating the model slope, we copy the gravity slope and
    rotate 90 degrees. MUCH EASIER

--- app/components/gravity_status_component.rb
+++ app/components/gravity_status_component.rb
@@ -3,12 +3,11 @@
 class GravityStatusComponent < ViewComponent::Base
   delegate :defined_gravity, :using_default?, to: :status_model
 
-  attr_reader :status_model, :validation_run, :origin, :text_offset, :arrow_size, :axis_length
+  attr_reader :status_model, :validation_run, :text_offset, :arrow_size, :axis_length
 
   def initialize(validation_run:)
     @validation_run = validation_run
     @status_model = GravityStatus.new(validation_run)
-    @origin = 0
     @axis_length = 125
     @text_offset = 10
     @arrow_size = 10
@@ -16,74 +15,68 @@ class GravityStatusComponent < ViewComponent::Base
 
   def slope_direction
     if status_model.scope.gvec_x.positive?
-      "sloping downwards"
-    elsif status_model.scope.gvec_x.negative?
       "sloping upwards"
+    elsif status_model.scope.gvec_x.negative?
+      "sloping downwards"
     else
       "flat"
     end
   end
 
-  def model_slope_line
+  # def model_slope_line
+  #   multiplyer = 10
+  #   gvec_x_normalised = (status_model.scope.gvec_x * multiplyer)
+  #   gvec_z_normalised = (status_model.scope.gvec_z * multiplyer)
+  #   line = <<~MY_DOC
+  #     <line x1='0' y1='0' stroke='green' stroke-width="3px">
+  #       <animate
+  #         attributeName="x2"
+  #         from="0"
+  #         to="#{gvec_x_normalised}"
+  #         dur="1s"
+  #         fill="freeze"
+  #         begin="1s"
+  #          />
+  #       <animate
+  #         attributeName="y2"
+  #         from="0"
+  #         to="#{gvec_z_normalised}"
+  #         dur="1s"
+  #         fill="freeze"
+  #         begin="2s"
+  #          />
+
+  #          <animate
+  #         attributeName="x1"
+  #         from="0"
+  #         to="#{gvec_x_normalised}"
+  #         dur="1s"
+  #         fill="freeze"
+  #         begin="1s"
+  #          />
+  #       <animate
+  #         attributeName="y1"
+  #         from="0"
+  #         to="#{gvec_z_normalised}"
+  #         dur="1s"
+  #         fill="freeze"
+  #         begin="2s"
+  #          />
+  #     </line>
+  #   MY_DOC
+
+  #   line.html_safe
+  # end
+
+  def gravity_line(colour="blue")
     multiplyer = 10
-
-    new_x2 = origin + ((status_model.scope.gvec_z * -1) * multiplyer)
-    new_z2 = origin + ((status_model.scope.gvec_x * -1) * multiplyer)
-
-    new_x1 = origin + ((status_model.scope.gvec_z) * multiplyer)
-    new_z1 = origin + ((status_model.scope.gvec_x) * multiplyer)
-
+    gvec_x_normalised = (status_model.scope.gvec_x * multiplyer)
+    gvec_z_normalised = ((status_model.scope.gvec_z ) * multiplyer)
     line = <<~MY_DOC
-      <line x1='#{origin}' y1='#{origin}' x2='#{origin}' y2='#{origin}' stroke='green' stroke-width="3px">
+      <line x1='0' y1='0' stroke='#{colour}' stroke-width="3px">
         <animate
           attributeName="x2"
-          from="#{origin}"
-          to="#{new_x2}"
-          dur="1s"
-          fill="freeze"
-          begin="1s"
-           />
-        <animate
-          attributeName="y2"
-          from="#{origin}"
-          to="#{new_z2}"
-          dur="1s"
-          fill="freeze"
-          begin="2s"
-           />
-
-          <animate
-           attributeName="x1"
-           from="#{origin}"
-           to="#{new_x1}"
-           dur="1s"
-           fill="freeze"
-           begin="1s"
-            />
-         <animate
-           attributeName="y1"
-           from="#{origin}"
-           to="#{new_z1}"
-           dur="1s"
-           fill="freeze"
-           begin="2s"
-            />
-      </line>
-    MY_DOC
-
-    line.html_safe
-  end
-
-  def gravity_line
-    multiplyer = 10
-    gvec_x_normalised = origin + (status_model.scope.gvec_x * multiplyer)
-    gvec_z_normalised = origin + ((status_model.scope.gvec_z * -1) * multiplyer)
-
-    line = <<~MY_DOC
-      <line x1='#{origin}' y1='#{origin}' x2='#{origin}' y2='#{origin}' stroke='blue' stroke-width="3px">
-        <animate
-          attributeName="x2"
-          from="#{origin}"
+          from="0"
           to="#{gvec_x_normalised}"
           dur="1s"
           fill="freeze"
@@ -91,7 +84,7 @@ class GravityStatusComponent < ViewComponent::Base
            />
         <animate
           attributeName="y2"
-          from="#{origin}"
+          from="0"
           to="#{gvec_z_normalised}"
           dur="1s"
           fill="freeze"
