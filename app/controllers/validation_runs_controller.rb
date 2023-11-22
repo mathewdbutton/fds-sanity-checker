aefde69aeb1d43a7c11bf0ab170ceb4554e64d8c
Mathew Button <mat@pushbutton.xyz>
2023-11-22 15:59:32 +1100

    Update validation_runs_controller.rb
    This cleaning job clearly isn't working as well as I'd hoped

--- app/controllers/validation_runs_controller.rb
+++ app/controllers/validation_runs_controller.rb
@@ -1,5 +1,5 @@
 class ValidationRunsController < ApplicationController
-  before_action -> { ValidationCleanup.call(beginning_at: Time.current) }
+  # before_action -> { ValidationCleanup.call(beginning_at: Time.current) }
 
   def show
     @validation_run = ValidationRun.find_by(run_id: params[:run_id])
