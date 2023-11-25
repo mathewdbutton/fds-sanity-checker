b140a2166a4e385c7ed6cc53ca18472b615a304a
Mathew Button <mat@pushbutton.xyz>
2023-11-25 16:21:16 +1100

    Store FDS model file against validation run

--- app/controllers/model_loader_controller.rb
+++ app/controllers/model_loader_controller.rb
@@ -4,6 +4,7 @@ class ModelLoaderController < ApplicationController
 
   def create
     ModelPipeline.call(params[:model])
+    CurrentValidation.validation_run.update!(fds_model: params[:model])
 
     redirect_to validation_runs_show_path(run_id: CurrentValidation.validation_run.run_id)
   end
